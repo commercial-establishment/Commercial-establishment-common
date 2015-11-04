94c90b5eeb310daa7e8b9051f47bf03543d42bb1
commercial-establishment <commercial.establishment@gmail.com>
2015-11-04 16:23:22 +0600

    Added services and repositories

--- src/main/java/kz/hts/ce/entity/Admin.java
+++ src/main/java/kz/hts/ce/entity/Admin.java
@@ -163,22 +163,4 @@ public class Admin extends BaseEntity {
     public void setBlocked(boolean blocked) {
         this.blocked = blocked;
     }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", phone='" + phone + '\'' +
-                ", gender=" + gender +
-                ", startWorkDate=" + startWorkDate +
-                ", endWorkDate=" + endWorkDate +
-                ", role=" + role +
-                ", blocked=" + blocked +
-                '}';
-    }
 }
